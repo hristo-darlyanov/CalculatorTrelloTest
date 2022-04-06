d2d74f2ffc13ddda403f64698d9e336c15141b41
hristo-darlyanov <memeda963@gmail.com>
2022-04-06 14:56:29 +0300

    Added logic for numeric input buttons

--- CalculatorTrelloTest/View/CalculatorView.cs
+++ CalculatorTrelloTest/View/CalculatorView.cs
@@ -16,5 +16,55 @@ namespace CalculatorTrelloTest.View
         {
             InitializeComponent();
         }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            txtBox.Text = "0";
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            txtBox.Text += "1";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            txtBox.Text += "2";
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            txtBox.Text += "3";
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            txtBox.Text += "4";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            txtBox.Text += "5";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            txtBox.Text += "6";
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            txtBox.Text += "7";
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            txtBox.Text += "8";
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            txtBox.Text += "9";
+        }
     }
 }
