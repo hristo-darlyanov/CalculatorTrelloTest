fd9f04fbf536e6455bb3a700440ca34522309213
hristo-darlyanov <memeda963@gmail.com>
2022-04-08 14:04:19 +0300

    Fixed bugs for text input and changed forms name from CalculatorView to Calculator

--- CalculatorTrelloTest/View/CalculatorView.cs
+++ CalculatorTrelloTest/View/CalculatorView.cs
@@ -22,42 +22,52 @@ namespace CalculatorTrelloTest.View
         {
             txtBox.Text += "0";
         }
+
         private void btn1_Click(object sender, EventArgs e)
         {
             txtBox.Text += "1";
         }
+
         private void btn2_Click(object sender, EventArgs e)
         {
             txtBox.Text += "2";
         }
+
         private void btn3_Click(object sender, EventArgs e)
         {
             txtBox.Text += "3";
         }
+
         private void btn4_Click(object sender, EventArgs e)
         {
             txtBox.Text += "4";
         }
+
         private void btn5_Click(object sender, EventArgs e)
         {
             txtBox.Text += "5";
         }
+
         private void btn6_Click(object sender, EventArgs e)
         {
             txtBox.Text += "6";
         }
+
         private void btn7_Click(object sender, EventArgs e)
         {
             txtBox.Text += "7";
         }
+
         private void btn8_Click(object sender, EventArgs e)
         {
             txtBox.Text += "8";
         }
+
         private void btn9_Click(object sender, EventArgs e)
         {
             txtBox.Text += "9";
         }
+
         private void btnSquareRoot_Click(object sender, EventArgs e)
         {
             if (txtBox.Text.Length > 0)
@@ -67,6 +77,7 @@ namespace CalculatorTrelloTest.View
                 txtBox.Text = squareRoot.ToString();
             }
         }
+
         private void btnPlus_Click(object sender, EventArgs e)
         {
             if (txtBox.Text.Length > 0)
@@ -77,6 +88,7 @@ namespace CalculatorTrelloTest.View
                 count = 2;
             }
         }
+
         private void btnOnOff_Click(object sender, EventArgs e)
         {
             if (txtBox.Text.Length > 0)
@@ -84,99 +96,209 @@ namespace CalculatorTrelloTest.View
                 txtBox.Clear();
             }
         }
+
         private void btnEquals_Click(object sender, EventArgs e)
         {
             Compute(count);
         }
+
         private void btnMinus_Click(object sender, EventArgs e)
         {
             if (txtBox.Text.Length > 0)
             {
-                num1 = double.Parse(txtBox.Text);
-                txtBox.Clear();
-                txtBox.Focus();
-                count = 1;
+                if (double.TryParse(txtBox.Text, out var x))
+                {
+                    num1 = double.Parse(txtBox.Text);
+                    txtBox.Clear();
+                    txtBox.Focus();
+                    count = 1;
+                }
+                else
+                {
+                    MessageBox.Show("Please use number");
+                }
             }
         }
+
         private void btnMultipy_Click(object sender, EventArgs e)
         {
             if (txtBox.Text.Length > 0)
             {
-                num1 = double.Parse(txtBox.Text);
-                txtBox.Clear();
-                txtBox.Focus();
-                count = 3;
+                if (double.TryParse(txtBox.Text, out var x))
+                {
+                    num1 = double.Parse(txtBox.Text);
+                    txtBox.Clear();
+                    txtBox.Focus();
+                    count = 3;
+                }
+                else
+                {
+                    MessageBox.Show("Please use number");
+                }
             }
         }
+
         private void btnDivison_Click(object sender, EventArgs e)
         {
             if (txtBox.Text.Length > 0)
             {
-                num1 = double.Parse(txtBox.Text);
-                txtBox.Clear();
-                txtBox.Focus();
-                count = 4;
+                if (double.TryParse(txtBox.Text, out var x))
+                {
+                    num1 = double.Parse(txtBox.Text);
+                    txtBox.Clear();
+                    txtBox.Focus();
+                    count = 4;
+                }
+                else
+                {
+                    MessageBox.Show("Please use number");
+                }
             }
         }
+
         private void btnPercent_Click(object sender, EventArgs e)
         {
             if (txtBox.Text.Length > 0)
             {
-                num1 = double.Parse(txtBox.Text);
-                txtBox.Clear();
-                txtBox.Focus();
-                count = 5;
+                if (double.TryParse(txtBox.Text, out var x))
+                {
+                    num1 = double.Parse(txtBox.Text);
+                    txtBox.Clear();
+                    txtBox.Focus();
+                    count = 5;
+                }
+                else
+                {
+                    MessageBox.Show("Please use number");
+                }
             }
         }
+
         private void btnPower_Click(object sender, EventArgs e)
         {
             if (txtBox.Text.Length > 0)
             {
-                num1 = double.Parse(txtBox.Text);
-                txtBox.Clear();
-                txtBox.Focus();
-                count = 6;
+                if (double.TryParse(txtBox.Text, out var x))
+                {
+                    num1 = double.Parse(txtBox.Text);
+                    txtBox.Clear();
+                    txtBox.Focus();
+                    count = 6;
+                }
+                else
+                {
+                    MessageBox.Show("Please use number!");
+                }
             }
         }
+
         private void btnPlusMinus_Click(object sender, EventArgs e)
         {
             if (txtBox.Text.Length > 0)
             {
-                double reverseSign = double.Parse(txtBox.Text);
-                reverseSign = reverseSign - reverseSign * 2;
-                txtBox.Text = reverseSign.ToString();
+                if (double.TryParse(txtBox.Text, out var x))
+                {
+                    double reverseSign = double.Parse(txtBox.Text);
+                    reverseSign = reverseSign - reverseSign * 2;
+                    txtBox.Text = reverseSign.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Please use number");
+                }
             }
         }
+
         public void Compute(int count)
         {
             switch (count)
             {
                 case 1:
-                    ans = num1 - float.Parse(txtBox.Text);
-                    txtBox.Text = ans.ToString();
+                    if (txtBox.Text.Length > 0)
+                    {
+                        if (double.TryParse(txtBox.Text, out var x))
+                        {
+                            ans = num1 - float.Parse(txtBox.Text);
+                            txtBox.Text = ans.ToString();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Please use number!");
+                        }
+                    }
                     break;
                 case 2:
-                    ans = num1 + float.Parse(txtBox.Text);
-                    txtBox.Text = ans.ToString();
+                    if (txtBox.Text.Length > 0)
+                    {
+                        if (double.TryParse(txtBox.Text, out var x))
+                        {
+                            ans = num1 + float.Parse(txtBox.Text);
+                            txtBox.Text = ans.ToString();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Please use number!");
+                        }
+                    }
                     break;
                 case 3:
-                    ans = num1 * float.Parse(txtBox.Text);
-                    txtBox.Text = ans.ToString();
+                    if (txtBox.Text.Length > 0)
+                    {
+                        if (double.TryParse(txtBox.Text, out var x))
+                        {
+                            ans = num1 * float.Parse(txtBox.Text);
+                            txtBox.Text = ans.ToString();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Please use number!");
+                        }
+                    }
                     break;
                 case 4:
-                    ans = num1 / float.Parse(txtBox.Text);
-                    txtBox.Text = ans.ToString();
+                    if (txtBox.Text.Length > 0)
+                    {
+                        if (double.TryParse(txtBox.Text, out var x))
+                        {
+                            ans = num1 / float.Parse(txtBox.Text);
+                            txtBox.Text = ans.ToString();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Please use number!");
+                        }
+                    }
                     break;
                 case 5:
-                    ans = num1 % float.Parse(txtBox.Text);
-                    txtBox.Text = ans.ToString();
+                    if (txtBox.Text.Length > 0)
+                    {
+                        if (double.TryParse(txtBox.Text, out var x))
+                        {
+                            ans = num1 % float.Parse(txtBox.Text);
+                            txtBox.Text = ans.ToString();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Please use number!");
+                        }
+                    }
                     break;
                 case 6:
-                    ans = Math.Pow(num1,float.Parse(txtBox.Text));
-                    txtBox.Text = ans.ToString();
+                    if (txtBox.Text.Length > 0)
+                    {
+                        if (double.TryParse(txtBox.Text, out var x))
+                        {
+                            ans = Math.Pow(num1,float.Parse(txtBox.Text));
+                            txtBox.Text = ans.ToString();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Please use number!");
+                        }
+                    }
                     break;
                 default:
-                    break;
+                    break;                   
             }
         }
     }
